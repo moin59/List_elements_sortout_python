664ae452da4a621034ac4c6bb09a3d5811ce3daf
moin59 <eee.moin@gmail.com>
2020-02-22 10:12:47 +0100

    modify: variable name

--- test.py
+++ test.py
@@ -5,14 +5,12 @@ import sort.list_sorting as srt
 class TestProcessFunction(unittest.TestCase):
 
     def setUp(self):
-        self.list_ = [4, 7, 19, 95, 50, 71, 11, 133, 1, 99, 59]
-        self.list_out = [1, 4, 7, 11, 19, 50, 59, 71, 95, 99, 133]
+        self.input_list = [4, 7, 19, 95, 50, 71, 11, 133, 1, 99, 59]
+        self.outout_list = [1, 4, 7, 11, 19, 50, 59, 71, 95, 99, 133]
         
     def test_list_sort(self):
-        list_update = srt.listsort(self.list_)
-        self.assertListEqual(self.list_out, list_update)
+        list_update = srt.listsort(self.input_list)
+        self.assertListEqual(self.outout_list, list_update)
         
-
-
 if __name__ == '__main__':
     unittest.main() 
